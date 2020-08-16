fd5c6517bbf2ca4c4ab6e4c6707c4ef446283181
Michael Schwieger <michael_schwieger@outlook.at>
2020-08-16 10:18:54 +0200

    Did a thing

--- run.sh
+++ run.sh
@@ -1,8 +1,30 @@
 #!/bin/bash
 
-cd "$(dirname "$BASH_SOURCE")"
+echo "This script is really basic! It won't close anything on its own so you will have to do it yourself when not using lavalink or the bot, kthxbye"
 
+if java --version; then
+     echo “Success, Java is installed.”
+else
+     echo “Failure, exit status: $?”
+fi
+
+
+echo "Starting lavalink server as background process..."
 nohup java -jar lavalink/Lavalink.jar &
 
-adb/bin/python launcher.py &
+echo "Waiting 15 seconds to give lavalink enough time to start..."
+sleep 15
 
+echo "Checking for python venv"
+
+venvdir="./adb/"
+ 
+url="some_url/file.tar.gz"
+file="${url##*/}"
+ 
+[ ! -d "$venvdir" ] && python -m venv "$dldir" && echo "Venv created... attempting to launch bot"
+ 
+ echo "Launching bot"
+
+ ./adb/bin/python launcher.py
+ 
