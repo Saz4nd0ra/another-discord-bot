45d5d7c5370c498ba1fd3c40ff68733ca9bd62d1
Michael Schwieger <michael_schwieger@outlook.at>
2020-12-27 15:19:19 +0100

    27/12/2020
    nsfw: fixed video issues with a dumb work around, added sources if available

--- core/cogs/nsfw/nsfw.py
+++ core/cogs/nsfw/nsfw.py
@@ -4,28 +4,47 @@ from .api import Rule34
 from ...utils import checks
 import asyncio
 import random
+import io
 from ...utils.exceptions import *
 from ...utils.embed import Embed
 from discord.ext import commands
+
+VIDEO_FORMATS = [
+    "mp4",
+    "webm",
+    # and so on, I don't really know which formats r34 uses
+]
 
 
 class NSFW(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = self.bot.config
-        self.l = asyncio.get_event_loop()
-        self.rule34 = api.Rule34(self.l)
+        self.loop = asyncio.get_event_loop()
+        self.rule34 = api.Rule34(self.loop)
 
-
-# TODO work on a blacklist system and user configs (yikes)
+    # TODO work on a blacklist system and user configs (yikes)
     @checks.is_nsfw_channel()
     @commands.command()
     async def r34(self, ctx, *, search):
 
         images = await self.rule34.getImages(tags=search)
+        file = images[random.randint(0, len(images))]
 
-        embed = Embed(ctx, title="Image found.", image=images[random.randint(0, len(images))].file_url)
-        await ctx.send(embed=embed)
+        # TODO work on filtering videos, so we can actually send them
+        if any(x in file.file_url for x in VIDEO_FORMATS):
+            embed = Embed(ctx, title="Video found.")
+            if file.source:
+                embed.add_field(name="Source:", value=f"[Click Here!]({file.source})")
+            await ctx.send(embed=embed)
+            await ctx.send(file.file_url)
+        else:
+            embed = Embed(ctx, title="Image found.", image=images[random.randint(0, len(images))].file_url)
+            if file.source:
+                embed.add_field(name="Source:", value=f"[Click Here!]({file.source})")
+            await ctx.send(embed=embed)
+
+
 
 
 def setup(bot):
