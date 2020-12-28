a489f5cf5b3d627fb6837451ea1288111125dae7
Michael Schwieger <michael_schwieger@outlook.at>
2020-12-28 13:58:46 +0100

    28/12/2020
    nsfw & api: finished danbooru command, working on saucenao command now

--- core/cogs/nsfw/api.py
+++ core/cogs/nsfw/api.py
@@ -1,5 +1,5 @@
 import rule34
-import pybooru as danbooru
+from pybooru import Danbooru
 import asyncio
 import random
 
@@ -11,13 +11,13 @@ VIDEO_FORMATS = [
 ]
 
 
-class Rule34:
+class Rule34API:
 
     def __init__(self):
         self.loop = asyncio.get_event_loop()
         self.rule34 = rule34.Rule34(self.loop)
 
-    async def get_random_post_url(self, search):
+    async def get_random_r34(self, search):
 
         images = await self.rule34.getImages(tags=search)
         try:
@@ -36,3 +36,36 @@ class Rule34:
             has_source = False
 
         return file, is_video, has_source
+
+
+class DanbooruAPI:
+
+    def __init__(self):
+        self.danbooru = Danbooru("danbooru")
+
+    def get_random_danbooru(self, search):
+
+        images = self.danbooru.post_list(tags=search, limit=100)
+
+        try:
+            file = images[random.randint(0, len(images) - 1)]
+        except TypeError:
+            return
+
+        if any(x in file["file_url"] for x in VIDEO_FORMATS):
+            is_video = True
+        else:
+            is_video = False
+
+        if file["source"]:
+            has_source = True
+        else:
+            has_source = False
+
+        return file, is_video, has_source
+
+
+class SauceNAO:
+
+    def __init__(self):
+        pass
--- core/cogs/nsfw/nsfw.py
+++ core/cogs/nsfw/nsfw.py
@@ -1,9 +1,9 @@
 import discord
-from .api import Rule34
+from .api import Rule34API
+from .api import DanbooruAPI
 from ...utils import checks
 import asyncio
 import random
-import pybooru as danbooru
 from ...utils.exceptions import *
 from ...utils.embed import Embed
 from discord.ext import commands
@@ -13,14 +13,15 @@ class NSFW(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = self.bot.config
-        self.rule34 = Rule34()
+        self.rule34 = Rule34API()
+        self.danbooru = DanbooruAPI()
 
     # TODO work on a blacklist system and user configs (yikes)
     @checks.is_nsfw_channel()
     @commands.command()
     async def r34(self, ctx, *, search):
 
-        file, is_video, has_source = await self.rule34.get_random_post_url(search)
+        file, is_video, has_source = await self.rule34.get_random_r34(search)
         # TODO work on filtering videos, so we can actually send them
         if is_video:
             embed = Embed(ctx, title="Video found.", thumbnail=file.preview_url)
@@ -36,7 +37,21 @@ class NSFW(commands.Cog):
     @commands.command()
     async def danbooru(self, ctx, *, search):
 
-        await ctx.send("Not working yet.")
+        file, is_video, has_source = self.danbooru.get_random_danbooru(search)
+
+        file_source = file["source"]
+        file_preview = file["preview_file_url"]
+        file_url = file["file_url"]
+        # TODO work on filtering videos, so we can actually send them
+        if is_video:
+            embed = Embed(ctx, title="Video found.", thumbnail=file_preview)
+        else:
+            embed = Embed(ctx, title="Image found.", image=file_url)
+        if has_source:
+            embed.add_field(name="Source:", value=f"[Click Here!]({file_source})")
+        embed.add_field(name="Image/Video:", value=f"[Click Here!]({file_url})")
+
+        await ctx.send(embed=embed)
 
 
 def setup(bot):
